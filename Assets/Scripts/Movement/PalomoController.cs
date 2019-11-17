e713730ca0077bd08005724207cacd48b54eb836
Christian <chrpefe@gmail.com>
2019-11-17 01:58:03 +0100

    Añadida la mecanica de caminar
    Se ha añadido la mecanica de caminar. Si miras al suelo palomo se detiene, si miras hacia arriba palomo vuelve a andar y si miras hacia arriba durante varios segundos comienza a volar

--- Assets/Scripts/Movement/PalomoController.cs
+++ Assets/Scripts/Movement/PalomoController.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using UnityEngine;
 
-public class PalomoController : MonoBehaviour {
+public class PalomoController : MonoBehaviour
+{
     public float TurnForceMultiplier = 1;
     public float MaxSpeed = 50.0f;
+    public float StopAngle;
+    public float MoveAngle;
+    public float FlyAngle;
+    public float DelayTimeFly = 2f;
 
     private Rigidbody _rigidBody;
     private Camera _mainCamera;
@@ -11,17 +16,29 @@ public class PalomoController : MonoBehaviour {
     private bool _flying;
     private bool _grounded;
 
-    void Start () {
+    private bool _moveForwardGround;
+    private float _elapsedTimeFly=0f;
+
+
+
+    void Start()
+    {
         _rigidBody = GetComponent<Rigidbody>();
         _mainCamera = Camera.main;
 
-        _flying = true;
-        _grounded = false;
+        _flying = false;
+        _grounded = true;
+        _moveForwardGround = true;
+        MoveAngle = MoveAngle * 0.01f;
+        StopAngle = StopAngle * 0.01f;
+        FlyAngle = FlyAngle * 0.01f;
     }
 
-	// Update is called once per frame
-	void Update () {
-        if (_flying) {
+    // Update is called once per frame
+    void Update()
+    {
+        if (_flying)
+        {
             var forward = _mainCamera.transform.forward;
             //var right = _mainCamera.transform.right;
 
@@ -31,32 +48,71 @@ public class PalomoController : MonoBehaviour {
             var desiredMoveDirection = forward;
             transform.Translate(desiredMoveDirection * MaxSpeed * Time.deltaTime);
             transform.forward = desiredMoveDirection;
-        } else {
-            if (!_grounded) {
+        }
+        else
+        {
+            
+            if (!_grounded)
+            {
                 _rigidBody.useGravity = true;
                 _rigidBody.AddForce(1, -500, 1);
-            } else {
+            }
+            else
+            {
                 transform.rotation = Quaternion.Euler(0, 0, 0);
                 _rigidBody.mass = 2;
 
                 // controller andando
+                if (_mainCamera.transform.forward.y <= StopAngle)
+                {
+                    _moveForwardGround = false;
+                }
+
+                if (_mainCamera.transform.forward.y >= MoveAngle && _mainCamera.transform.forward.y < FlyAngle)
+                {
+                    _moveForwardGround = true;
+                }
+
+                if(_mainCamera.transform.forward.y >= FlyAngle)
+                {
+                    _elapsedTimeFly += Time.deltaTime;
+                    if (_elapsedTimeFly >= DelayTimeFly)
+                        _flying = true;
+                    print(_flying);
+
+                }
+                else
+                {
+                    _elapsedTimeFly = 0;
+                }
+
+                if (_moveForwardGround)
+                {
+                    transform.position = transform.position + _mainCamera.transform.forward * MaxSpeed * Time.deltaTime;
+                }
             }
         }
     }
 
     void FixedUpdate()
     {
-        if (_flying) {
-            
+        if (_flying)
+        {
+
         }
     }
 
-    void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.tag.Contains("Ground")) {
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Contains("Ground"))
+        {
             _grounded = true;
+            _flying = false;
         }
 
-        _flying = false;
+        
     }
+
+
+   
 }
-    
