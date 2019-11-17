34c4d2105cdfd314b1b14af1894e94b388d34561
Christian <chrpefe@gmail.com>
2019-11-17 17:32:30 +0100

    Realizada los scripts necesarios para la interacción con elementos

---
+++ Assets/Scripts/CajaMoverPrueba.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaMoverPrueba : MonoBehaviour,Interactuable
+{
+    public void Execute()
+    {
+        transform.position = transform.position+new Vector3(0,0,-2);
+    }
+
+}
---
+++ Assets/Scripts/VRGaze.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VRGaze : MonoBehaviour
+{
+    public float totalTime = 1.5f;
+    bool gvrStatus;
+    float gvrTimer;
+
+    public int distanceOfRay = 10;
+    private RaycastHit _hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gvrStatus)
+        {
+            gvrTimer += Time.deltaTime;
+            float elapsed = gvrTimer / totalTime;
+            Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+            if (elapsed >= totalTime && Physics.Raycast(ray, out _hit, distanceOfRay))
+            {
+                
+                //if(_hit.transform.gameObject.GetComponent<Interactuable>()!=null)
+                    _hit.transform.gameObject.GetComponent<Interactuable>().Execute();
+                //print(_hit.transform.gameObject);
+                GVROff();
+            }
+        }
+    }
+
+    public void GVROn()
+    {
+        gvrStatus = true;
+    }
+
+    public void GVROff()
+    {
+        gvrStatus = false;
+        gvrTimer = 0;
+    }
+}
