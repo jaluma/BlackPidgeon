559342506b7a4ade40ce38d28071158f81ed9c78
Gema Rico Pozas <gemarico@outlook.it>
2019-11-19 17:37:14 +0100

    Npc fixed

---
+++ Assets/scripts/CreateNPC.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class CreateNPC : MonoBehaviour
+{
+
+    public NavMeshAgent agent;
+    public NavMeshAgent agent1;
+    public NavMeshAgent agent2;
+    public NavMeshAgent agent3;
+    public NavMeshAgent agent4;
+    public NavMeshAgent agent5;
+    public NavMeshAgent agent6;
+
+    private int npcCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GenerateNPC());
+    }
+
+    // Update is called once per frame
+    IEnumerator GenerateNPC()
+    {
+        while (npcCount < 10)
+        {
+            Instantiate(agent, RandomNavmeshLocation(50), Quaternion.identity);
+            Instantiate(agent1, RandomNavmeshLocation(50), Quaternion.identity);
+            Instantiate(agent2, RandomNavmeshLocation(50), Quaternion.identity);
+            Instantiate(agent3, RandomNavmeshLocation(50), Quaternion.identity);
+            Instantiate(agent4, RandomNavmeshLocation(50), Quaternion.identity);
+            Instantiate(agent5, RandomNavmeshLocation(50), Quaternion.identity);
+            Instantiate(agent6, RandomNavmeshLocation(50), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);
+            npcCount++;
+        }
+    }
+
+    public Vector3 RandomNavmeshLocation(float radius)
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * radius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+        if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1))
+        {
+            finalPosition = hit.position;
+        }
+        return finalPosition;
+    }
+}
--- Assets/scripts/Patrol.cs
+++ Assets/scripts/Patrol.cs
@@ -6,44 +6,34 @@ using System.Collections;
 public class Patrol : MonoBehaviour
 {
 
-    public Transform[] points;
-    private int destPoint = 0;
     private NavMeshAgent agent;
 
 
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
-
-        // Disabling auto-braking allows for continuous movement
-        // between points (ie, the agent doesn't slow down as it
-        // approaches a destination point).
+        agent = GetComponent<NavMeshAgent>();       
         agent.autoBraking = false;
+        agent.SetDestination(RandomNavmeshLocation(50f));        
 
-        GotoNextPoint();
     }
-
-
-    void GotoNextPoint()
-    {
-        // Returns if no points have been set up
-        if (points.Length == 0)
-            return;
-
-        // Set the agent to go to the currently selected destination.
-        agent.destination = points[destPoint].position;
-
-        // Choose the next point in the array as the destination,
-        // cycling to the start if necessary.
-        destPoint = (destPoint + 1) % points.Length;
-    }
-
 
     void Update()
     {
-        // Choose the next destination point when the agent gets
-        // close to the current one.
+       
         if (!agent.pathPending && agent.remainingDistance < 0.5f)
-            GotoNextPoint();
+        agent.SetDestination(RandomNavmeshLocation(50f));
+    }
+
+    public Vector3 RandomNavmeshLocation(float radius)
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * radius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+        if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1))
+        {
+            finalPosition = hit.position;
+        }
+        return finalPosition;
     }
 }
