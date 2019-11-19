0a22ebf57531938afe467d2dc76036c73f6bf267
Gema Rico Pozas <gemarico@outlook.it>
2019-11-19 20:10:44 +0100

    Interaccion and demoscene integrated

---
+++ Assets/scripts/DesactivarPrueba.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesactivarPrueba : MonoBehaviour,Interactuable
+{
+
+
+    public void Desactivar()
+    {
+        gameObject.SetActive(false);
+        print("CiudadanoMorir");
+    }
+
+    public void Execute()
+    {
+        Desactivar();
+    }
+}
