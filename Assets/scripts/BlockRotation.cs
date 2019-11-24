90c3e1b9a58db8e42edccb02dcfdf4fa2f737398
Javier Martínez Álvarez <javiermartinezalvarez98@gmail.com>
2019-11-24 02:05:48 +0100

    fix sky attack, fix menu (flying), remove useless code

--- Assets/scripts/BlockRotation.cs
+++ Assets/scripts/BlockRotation.cs
@@ -3,25 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BlockRotation : MonoBehaviour {
-    private Quaternion _rotation;
     public GameObject Parent;
-
     public bool Block = false;
-    private Transform positionInitial;
+
+    private Transform _positionInitial;
 
     private void Start() {
-        positionInitial = transform;
+        _positionInitial = transform;       // no usar referencias. FALLA!
     }
 
     void Update() {
         if (Block) {
             transform.parent = GameObject.FindGameObjectWithTag("Player").transform;
-            //Camera.main.GetComponent<LookingDown>().UpdateLastShowed();
+            Camera.main.GetComponent<LookingDown>().UpdateLastShowed();
         } else {
-            transform.localPosition = new Vector3(0.1f, 0.36f, 0.72f);
+            transform.localPosition = new Vector3(0.1f, 0.36f, 0.72f);          // valores por defecto del canvas.
             transform.parent = Camera.main.transform;
             transform.rotation = new Quaternion(0, Camera.main.transform.rotation.y, 0, Camera.main.transform.rotation.w);
-            //transform.rotation = Camera.main.transform.rotation;
         }
     }
 }
--- Assets/scripts/LookingDown.cs
+++ Assets/scripts/LookingDown.cs
@@ -3,31 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 public class LookingDown : MonoBehaviour {
-    private bool menuOut;
-    GameObject[] menuElemets;
+
     public int TimeOutofMenu = 0;
     public int TimeLimit = 300;
     public float ToleranciaMirarSuelo = 0.766f;
     public float Ralentizacion = 0.3f;
-    GameObject copyCanvas;
     public float Distance = 0.7f;
     public float Altura = 1.1F;
     public float YRotationCamera;
     public float YRotationCanvas;
     public long SecondDelay = 2;
-
-    private DateTime lastShow;
-    private float _alturaDefault;
-
-    GameObject player;
     public bool Flying = false;
+
+    private float _alturaDefault;
+    private bool _menuOut;
+    private GameObject[] _menuElemets;
+    private GameObject copyCanvas;
+    private DateTime _lastShow;
 
     // Start is called before the first frame update
     void Start() {
-        menuElemets = GameObject.FindGameObjectsWithTag("MenuElements");
+        _menuElemets = GameObject.FindGameObjectsWithTag("MenuElements");
         copyCanvas = GameObject.FindGameObjectWithTag("Canvas");
-
-        player = GameObject.FindGameObjectWithTag("Player");
 
         _alturaDefault = Altura;
 
@@ -37,81 +34,55 @@ public class LookingDown : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        // Debug.Log(TimeOutofMenu);
-        //Debug.Log("x:" + gameObject.transform.eulerAngles.x);
-        // Debug.Log("y:" + gameObject.transform.eulerAngles.y);
-        //Debug.Log("z:" + gameObject.transform.eulerAngles.z);
-        // Debug.Log(gameObject.transform.rotation.ToString());
-
         if (Vector3.Dot(gameObject.transform.forward, Vector3.down) > ToleranciaMirarSuelo) {
-            if (!menuOut) {
-                var now = GetTime(DateTime.Now);
-                var lastUpdate = GetTime(lastShow);
-                //if (lastUpdate + SecondDelay * 1000 >= now) {
+            if (!_menuOut) {
                 ShowMenu();
                 UpdateLastShowed();
-                //}
             }
-            Debug.Log("I'm looking down!");
 
         } else {
-            if (!Flying) {
-                if (Vector3.Dot(gameObject.transform.forward, Vector3.down) > 0.166f) {
-                    TimeOutofMenu = 0;
-                }
-                if (TimeOutofMenu >= TimeLimit) {
-                    HideMenu();
-                    Debug.Log("han pasado 5 segs");
-                    TimeOutofMenu = 0;
-                } else {
-                    TimeOutofMenu++;
-
-                }
+            if (Vector3.Dot(gameObject.transform.forward, Vector3.down) > 0.166f) {
+                TimeOutofMenu = 0;
+            }
+            if (TimeOutofMenu >= TimeLimit) {
+                HideMenu();
+                TimeOutofMenu = 0;
             } else {
-                if (!menuOut) {
-                    ShowMenu();
-                }
+                TimeOutofMenu++;
+
             }
 
         }
     }
 
     private void ShowMenu() {
-        //if (!Flying) {
-        //var position = new Vector3(transform.position.x, transform.position.y * Distance, transform.position.z);
-        //copyCanvas.transform.position = position;
-        //var rotation = Quaternion.LookRotation(transform.position);
-        //rotation *= Quaternion.Euler(0, transform.rotation.y, 0);
-        //copyCanvas.transform.rotation = Quaternion.Slerp(copyCanvas.transform.rotation, rotation, Time.deltaTime);
-        //var c = copyCanvas;
-        copyCanvas.GetComponent<BlockRotation>().Block = true;
-        var a = new Vector3(copyCanvas.transform.position.x, copyCanvas.transform.position.y + Altura, copyCanvas.transform.position.z);
-        copyCanvas.transform.position = a;
-        //c.transform.LookAt(c.transform.position + transform.rotation * Vector3.back, transform.rotation * Vector3.up);
+        var y = copyCanvas.transform.position.y;
 
-        if (!Flying) {
-            foreach (GameObject o in menuElemets) {
-                Time.timeScale = Ralentizacion;
-                o.SetActive(true);
-            }
+        foreach (GameObject o in _menuElemets) {
+            Time.timeScale = Ralentizacion;
+            o.SetActive(true);
         }
-        menuOut = true;
-        //}
+        if (!Flying) {
+            y += Altura;
+        }
+        copyCanvas.GetComponent<BlockRotation>().Block = true;
+        copyCanvas.transform.position = new Vector3(copyCanvas.transform.position.x, y, copyCanvas.transform.position.z);
+        _menuOut = true;
 
     }
 
     public void HideMenu() {
-        foreach (GameObject o in menuElemets) {
+        foreach (GameObject o in _menuElemets) {
             Time.timeScale = 1.0f;
             o.SetActive(false);
         }
-        menuOut = false;
+        _menuOut = false;
         copyCanvas.GetComponent<BlockRotation>().Block = false;
         UpdateLastShowed();
     }
 
     public void UpdateLastShowed() {
-        lastShow = DateTime.Now;
+        _lastShow = DateTime.Now;
     }
 
     public static long GetTime(DateTime time) {
