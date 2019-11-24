d5efb12e69f0e4f3b5324afba41ed5a26673a611
Javier Martínez Álvarez <javiermartinezalvarez98@gmail.com>
2019-11-24 13:33:44 +0100

    add npc pigeons

---
+++ Assets/scripts/ImagePigeons.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImagePigeons : MonoBehaviour {
+    private GameObject _canvas;
+    private int _count;
+    private GameObject _imageDefault;
+    private int _countPigeons;
+
+    // Start is called before the first frame update
+    void Start() {
+        _canvas = GameObject.FindGameObjectWithTag("CanvasOverlay");
+        // disable copy elements
+        _countPigeons = GameObject.FindGameObjectsWithTag("Pigeon").Length;
+        var images = GameObject.FindGameObjectsWithTag("ImagePigeon");
+        foreach (GameObject o in images) {
+            o.SetActive(false);
+        }
+        _imageDefault = images[0];
+        _count = 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    public void AddPigeon() {
+        // si es 0 significa que es la primera y hay que activar la de clonación
+        if (_count == 0) {
+            _imageDefault.SetActive(true);
+        } else {
+            if (_count < _countPigeons) {
+                var image = Object.Instantiate(_imageDefault).gameObject;
+                var copy = Instantiate(image, new Vector3(_imageDefault.transform.position.x + _count * 50, _imageDefault.transform.position.y, 0), Quaternion.identity);
+                copy.transform.parent = transform;
+                copy.SetActive(true);
+            }
+        }
+        _count++;
+    }
+}
---
+++ Assets/scripts/PigeonInteraction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigeonInteraction : MonoBehaviour, Interactuable {
+    private GameObject _canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _canvas = GameObject.FindGameObjectWithTag("CanvasOverlay");
+    }
+
+    public void Execute() {
+        _canvas.GetComponent<ImagePigeons>().AddPigeon();
+        Destroy(gameObject, 2);
+    }
+}
