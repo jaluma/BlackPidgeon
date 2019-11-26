9f1cf3da79f2a8f5b7e986f0dee20c6605a106a4
mglmeana <mglmeana@gmail.com>
2019-11-26 23:06:01 +0100

    "Replay" añadido. Cuando la caca impacta contra algo esta se adiere a ese objeto 2 segundos y despues vuelve a seguir al jugador, si no impacta contra nada a parte del suelo, vuelve automaticamente al jugador.

--- Assets/scripts/AtaqueAerio.cs
+++ Assets/scripts/AtaqueAerio.cs
@@ -8,6 +8,9 @@ public class AtaqueAerio : MonoBehaviour
     private GameObject _proyectil_Original;
     private GameObject _proyectilNuevo;
 
+    public CameraController CController;
+    public PalomoController PControler;
+
     public float VelocidadProyectil = 10f;
 
     private Rigidbody _rb;
@@ -29,6 +32,14 @@ public class AtaqueAerio : MonoBehaviour
         }*/
         
         _proyectilNuevo = Instantiate(_proyectil_Original);
+
+       
+
+        //replay
+        PControler.MaxSpeed = 0;
+        CController.ObjectToFollow = _proyectilNuevo;
+
+
         //_proyectil_Original.SetActive(false);
         _proyectilNuevo.transform.position = transform.position - new Vector3(0, 1, 0);
         _rb = _proyectilNuevo.GetComponent<Rigidbody>();
