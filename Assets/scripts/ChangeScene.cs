5b981d76cc7cd0cd94c8435fb3d94479487d7cdb
Christian <chrpefe@gmail.com>
2019-11-27 13:40:52 +0100

    Añadido teleport to TownHallScene

---
+++ Assets/scripts/ChangeScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public string sceneName = "TownHallScene";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if(other.CompareTag("Palomo"))
+        {
+            SceneManager.LoadScene("TownHallScene");
+        }
+    }
+
+    
+
+}
