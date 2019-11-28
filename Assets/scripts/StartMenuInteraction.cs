2805e29822f6f1f6619e3b8156e85df6e5660e8f
Javier Martínez <javiermartinezalvarez98@gmail.com>
2019-11-28 20:49:49 +0100

    add new look and feel start menu

--- Assets/scripts/StartMenuInteraction.cs
+++ Assets/scripts/StartMenuInteraction.cs
@@ -7,7 +7,7 @@ using UnityEngine.Events;
 public class StartMenuInteraction : MonoBehaviour {
 
     public float totalTime = 3;
-    public string scene = "Demo Scene";
+    public string scene = "";
     public float gvrTimer;
     private bool gvrStatus;
     public UnityEvent GVRClick;
