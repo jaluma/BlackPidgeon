1a2aa182ff46601bd679a3363edde5c45abb2070
Javier Martínez <javiermartinezalvarez98@gmail.com>
2019-11-28 20:44:34 +0100

    add credits and cinematic scenes

---
+++ Assets/scripts/Credits.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y > 3020)
+            SceneManager.LoadScene("StartMenu");
+    }
+}
