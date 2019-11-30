916999361a87c6207f1e31394229f621c6a744c5
Javier Martínez <javiermartinezalvarez98@gmail.com>
2019-11-30 17:37:17 +0100

    fix mayor

--- Assets/scripts/MayorShooting.cs
+++ Assets/scripts/MayorShooting.cs
@@ -7,22 +7,25 @@ public class MayorShooting : MonoBehaviour
 {
     public Rigidbody BulletPrefab;
     public GameObject EnemyToShoot;
-    private float _fireRate = 1;
-    private float _nextFire = 2;
+
+    private int count = 0;
+    private int _fireRate = 3;
 
     void Update()
     {
         transform.LookAt(EnemyToShoot.transform);
-        if (Time.time > _nextFire)
+        if (count++ >= _fireRate * 100)
         {
-            _nextFire = Time.time + _fireRate;
             ShootBullet();
+            count = 0;
         }
 
     }
     void ShootBullet()
     {
+        //new Vector3(transform.rotation.normalized.x, transform.rotation.normalized.y, transform.rotation.normalized.z)
         Rigidbody bullet = Instantiate(BulletPrefab, transform.position, transform.rotation) as Rigidbody;
+        bullet.AddForce(transform.forward.normalized * 1000);
         bullet.transform.parent = GameObject.FindGameObjectWithTag("BulletCollection").transform;
         Destroy(bullet.gameObject, 10f);
     }
